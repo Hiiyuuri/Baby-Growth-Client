afe89640826b90cebbed2fadbe3f2f8d55545b89
galihsatriaadhiputra <galihsadhira@gmail.com>
2022-07-19 22:31:43 +0700

    Feat
    Butto to edit Form

--- client/src/store/reducers/detailReducer.js
+++ client/src/store/reducers/detailReducer.js
@@ -2,14 +2,16 @@ import {
   PREGNANCY_DETAIL,
   MOTHER_DETAIL,
   MOTHER_PREGNANCY,
-  RECORDED_DATE
+  RECORDED_DATE,
+  BABY_ID
 } from "../actions/actionType";
 
 const initialState = {
   motherData: {},
   motherProfile: {},
   motherPregnancy: {},
-  recordedDate: ""
+  recordedDate: "",
+  babyId: 0
 };
 
 function detailReducer(state = initialState, action) {
@@ -22,6 +24,8 @@ function detailReducer(state = initialState, action) {
       return { ...state, motherPregnancy: action.payload };
     case RECORDED_DATE:
       return { ...state, recordedDate: action.payload };
+    case BABY_ID:
+      return { ...state, babyId: action.payload };
 
     default:
       return state;
