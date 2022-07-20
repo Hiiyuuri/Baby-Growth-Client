2b27713485eed9bfc66bff6488ba1632dde08ed8
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-07-20 21:38:24 +0700

    create navguard

--- client/src/App.js
+++ client/src/App.js
@@ -11,38 +11,130 @@ import RegisterAdmin from "./views/RegisterAdmin";
 import RegisterPregnancy from "./views/RegisterPregnancy";
 // import InputPregData from "./views/CreatePregnancyData";
 import MapMarkers from "./views/MapMarkers";
-import Modal from "./components/Modal/Modal"
+import Modal from "./components/Modal/Modal";
 import CreatePregnancyData from "./views/CreatePregnancyData";
 import InputBabyData from "./views/EditPregnancyData";
-import Login from './views/Login/Login';
+import Login from "./views/Login/Login";
 import MainPage from "./views/MainPage";
 import CreateBabyData from "./views/CreateBabyData";
 // import InputPregnancyData from "./views/EditBabyData";
 import EditBabyData from "./views/EditBabyData";
 import EditPregnancyData from "./views/EditPregnancyData";
+import NavigationGuard from "./views/navguard/NavigationGuard";
+import NavigationGuardLogin from "./views/navguard/NavigationGuardLogin";
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
-      <Route path="/" element={<MainPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/rt/:id" element={<ListPage />} />
-        <Route path="/mothers/:id" element={<MothersPage />} />
+        <Route
+          path="/"
+          element={
+            <NavigationGuard>
+              <MainPage />
+            </NavigationGuard>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <NavigationGuard>
+              <Dashboard />
+            </NavigationGuard>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <NavigationGuardLogin>
+              <Login />
+            </NavigationGuardLogin>
+          }
+        />
+        <Route
+          path="/rt/:id"
+          element={
+            <NavigationGuard>
+              <ListPage />
+            </NavigationGuard>
+          }
+        />
+        <Route
+          path="/mothers/:id"
+          element={
+            <NavigationGuard>
+              <MothersPage />
+            </NavigationGuard>
+          }
+        />
 
-        <Route path="/register-admin" element={<RegisterAdmin />} />
-        <Route path="/register-mom" element={<RegisterMom />} />
-        <Route path="/register-pregnancy" element={<RegisterPregnancy />} />
+        <Route
+          path="/register-admin"
+          element={
+            <NavigationGuard>
+              <RegisterAdmin />
+            </NavigationGuard>
+          }
+        />
+        <Route
+          path="/register-mom"
+          element={
+            <NavigationGuard>
+              <RegisterMom />
+            </NavigationGuard>
+          }
+        />
+        <Route
+          path="/register-pregnancy"
+          element={
+            <NavigationGuard>
+              <RegisterPregnancy />
+            </NavigationGuard>
+          }
+        />
 
-        <Route path="/create-preg-data" element={<CreatePregnancyData />} />
-        <Route path="/create-baby-data" element={<CreateBabyData />} />
+        <Route
+          path="/create-preg-data"
+          element={
+            <NavigationGuard>
+              <CreatePregnancyData />
+            </NavigationGuard>
+          }
+        />
+        <Route
+          path="/create-baby-data"
+          element={
+            <NavigationGuard>
+              <CreateBabyData />
+            </NavigationGuard>
+          }
+        />
 
-        <Route path="/edit-pregnancy-data/:PregnancyDataId" element={<EditPregnancyData />} />
-        <Route path="/edit-baby-data/:BabyDataId" element={<EditBabyData />} />
+        <Route
+          path="/edit-pregnancy-data/:PregnancyDataId"
+          element={
+            <NavigationGuard>
+              <EditPregnancyData />
+            </NavigationGuard>
+          }
+        />
+        <Route
+          path="/edit-baby-data/:BabyDataId"
+          element={
+            <NavigationGuard>
+              <EditBabyData />
+            </NavigationGuard>
+          }
+        />
 
-        <Route path="/map-markers" element={<MapMarkers />} />
+        <Route
+          path="/map-markers"
+          element={
+            <NavigationGuard>
+              <MapMarkers />
+            </NavigationGuard>
+          }
+        />
       </Routes>
     </div>
   );
